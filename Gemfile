14ffc39f6e6c6a084bd2fff088e5eaebb2d3240e
Eric Hutzelman <ehutzelman@gmail.com>
2010-09-20 16:24:18 -0500

    use bundler, update to rspec 2

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source :rubygems
+
+gem 'httparty', '~>0.5.0'
+gem 'happymapper', '~>0.3.2'
+
+group :development do
+  gem 'fakeweb', '~>1.2.5'
+  gem 'rspec', '~>2.0.pre'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
 require 'rubygems'
 require 'petfinder'
-require 'spec'
-require 'spec/autorun'
+require 'rspec'
 require 'fakeweb'
-
-Spec::Runner.configure do |config|  
-end
 
 def fixture_file(filename)
   File.read(File.dirname(__FILE__) + "/fixtures/#{filename}")
@@ -15,6 +12,6 @@ end
 
 def stub_get(url, filename, options={})
   opts = {:body => fixture_file(filename)}.merge(options)
-  
+
   FakeWeb.register_uri(:get, url, opts)
 end
